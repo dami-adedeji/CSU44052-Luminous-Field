fc1cb8cef238ae0ed2ce26ec6059b4c4d896f6c9
damiadedeji <adedejio@tcd.ie>
2025-07-14 14:19:57 +0100

    Plane and movement
    Here, I created an initial plane, which will be extended to be part of a tile function to create an infinite world. I included the Box struct from Lab1 to be a placeholder for the player. I implemented some basic movement with the WASD keys, and calculated boundaries so the box does not go past the borders of the plane.

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ static GLFWwindow *window;
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
 // OpenGL camera view parameters
-static glm::vec3 eye_center(0,0,0);
-static glm::vec3 lookat(0, 0, 1);
+static glm::vec3 eye_center(50,50,20);
+static glm::vec3 lookat(50, 0, -50);
 static glm::vec3 up(0, 1, 0);
 
 static GLuint LoadTextureTileBox(const char *texture_file_path) {
@@ -45,6 +45,303 @@ static GLuint LoadTextureTileBox(const char *texture_file_path) {
     return texture;
 }
 
+struct Plane
+{
+	glm::vec3 position, scale;
+	const char* texture_path;
+	GLuint shaderID;
+
+	GLfloat vertices[12] = {
+		0.0f, 0.0f, 0.0f,		// front left
+		0.0f, 0.0f, -1.0f,		// back right
+		1.0f, 0.0f, -1.0f,		// back left
+		1.0f, 0.0f, 0.0f		// front right
+	};
+
+	GLfloat colours[12] = {
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	GLuint indices[6] = {
+		0, 2, 1,
+		0, 3, 2
+	};
+
+
+	GLuint VAO, VBO, CBO, EBO, mvpMatrixID;
+
+	void initialise(glm::vec3 position, glm::vec3 scale, GLuint shaderID)
+	{
+		this->position = position;
+		this->scale = scale;
+		this->shaderID = shaderID;
+
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glGenBuffers(1, &CBO);
+		glBindBuffer(GL_ARRAY_BUFFER, CBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+
+		glGenBuffers(1, &EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		mvpMatrixID = glGetUniformLocation(shaderID, "MVP");
+
+		if (shaderID == 0)
+			std::cerr << "Failed to load plane shaders." << std::endl;
+	}
+
+	void render(glm::mat4 cameraMatrix)
+	{
+		glUseProgram(shaderID);
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, CBO);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glm::mat4 modelMatrix = glm::mat4(1.0f);
+		modelMatrix = glm::translate(modelMatrix, position);  // Apply translation
+		modelMatrix = glm::scale(modelMatrix, scale);         // Apply scaling
+		// Calculate MVP matrix
+		glm::mat4 MVP = cameraMatrix * modelMatrix;
+
+		// Send MVP matrix to the shader
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		//glEnableVertexAttribArray(2);
+		//glBindBuffer(GL_ARRAY_BUFFER, UVBO);
+		//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		// set texture sampler to use texture unit 0
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, textureID);
+		//shader->setUniform1i("textureSampler", 0);
+		//glUniform1i(textureSamplerID, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		//glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &CBO);
+		glDeleteBuffers(1, &EBO);
+		//glDeleteBuffers(1, &UVBO);
+		glDeleteVertexArrays(1,&VAO);
+		glDeleteProgram(shaderID);
+	}
+};
+
+struct Box {
+	glm::vec3 position;			// Position of the box
+	glm::vec3 scale;			// Size of the box in each axis
+	GLuint shaderID;
+
+	GLfloat vertex_buffer_data[72] = {	// Vertex definition for a canonical box
+		// Front face
+		-1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+
+		// Back face
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+
+		// Left face
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+
+		// Right face
+		1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Top face
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+
+		// Bottom face
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+
+		// Left, green
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Right, cyan
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+
+		// Top, blue
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 0.0f, 1.0f,
+	};
+
+	GLuint index_buffer_data[36] = {		// 12 triangle faces of a box
+		0, 1, 2,
+		0, 2, 3,
+
+		4, 5, 6,
+		4, 6, 7,
+
+		8, 9, 10,
+		8, 10, 11,
+
+		12, 13, 14,
+		12, 14, 15,
+
+		16, 17, 18,
+		16, 18, 19,
+
+		20, 21, 22,
+		20, 22, 23,
+	};
+
+	// OpenGL buffers
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint indexBufferID;
+	GLuint colorBufferID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+
+	void initialize(glm::vec3 position, glm::vec3 scale, GLuint shaderID) {
+		// Define scale of the box geometry
+		this->position = position;
+		this->scale = scale;
+		this->shaderID = shaderID;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(shaderID, "MVP");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(shaderID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// TODO: Model transform
+		// ------------------------------------
+		glm::mat4 modelMatrix = glm::mat4();
+		// Translate the box to its position
+		modelMatrix = glm::translate(modelMatrix, position);
+		// Scale the box along each axis
+		modelMatrix = glm::scale(modelMatrix, scale);
+		// rotate around the axis
+		//modelMatrix = glm::rotate(modelMatrix, (float)glfwGetTime(),rotateAxis);
+
+		// TODO: Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		// ------------------------------------
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteProgram(shaderID);
+	}
+};
+
+Box b;
+Plane p;
+
 int main(void) {
     // Initialise GLFW
 	if (!glfwInit())
@@ -59,7 +356,7 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "CSU44052 Supplemental Project - Dami Adedeji", NULL, NULL);
+	window = glfwCreateWindow(1024, 768, "hi", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cerr << "Failed to open a GLFW window." << std::endl;
@@ -80,17 +377,23 @@ int main(void) {
 		return -1;
 	}
 
+	GLuint objectShader = LoadShadersFromFile("../object.vert", "../object.frag");
+
 	// Background
-	glClearColor(0.5f, 0.5f, 0.6f, 0.0f);
+	glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
 	// shaders, objects, set up rest here
-	glm::float32 FoV = 45;
+	glm::float32 FoV = 60;
 	glm::float32 zNear = 0.1f;
 	glm::float32 zFar = 1000.0f;
 	glm::mat4 projectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, zNear, zFar);
+
+	p.initialise(glm::vec3(0,0,0), glm::vec3(100,1,100), objectShader);
+
+	b.initialize(glm::vec3(lookat.x, b.scale.y,lookat.z), glm::vec3(5,5,5), objectShader);
 
 // render
 	do
@@ -100,11 +403,10 @@ int main(void) {
 		// calculate viewMatrix and vp
 		glm::mat4 viewMatrix = glm::lookAt(eye_center, lookat, up);
 		glm::mat4 vp = projectionMatrix * viewMatrix;
-		glDepthMask(GL_FALSE);
-
-		// Render the skybox
-
-		//glDepthMask(GL_TRUE);
+		glDepthMask(GL_TRUE);
+		// render stuff here
+		p.render(vp);
+		b.render(vp);
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -113,7 +415,8 @@ int main(void) {
 	while (!glfwWindowShouldClose(window));
 
 	// Clean up
-
+	p.cleanup();
+	b.cleanup();
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 // cleanup
@@ -124,30 +427,28 @@ int main(void) {
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
-	if (key == GLFW_KEY_R && action == GLFW_PRESS)
+	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
-
+		if (b.position.z > -100.0f+b.scale.z)
+			b.position.z -= 1.0f;
 	}
 
-	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
+		if (b.position.z < (100.0f-b.scale.z))
+			b.position.z += 1.0f;
 	}
 
-	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
+		if (b.position.x > -100.0f+b.scale.x)
+			b.position.x -= 1.0f;
 	}
-
-	if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS))
+	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
-	}
-
-	if (key == GLFW_KEY_RIGHT && (action == GLFW_REPEAT || action == GLFW_PRESS))
-	{
-	}
-	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
-	}
-	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
-
+		if (b.position.x < (100.0f-b.scale.x))
+			b.position.x += 1.0f;
+		//std::cout << "Position: " << b.position.x  << std::endl;
 	}
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
---
+++ object.vert
@@ -0,0 +1,12 @@
+#version 330 core
+
+layout (location = 0) in vec3 vertexPos;
+layout (location = 1) in vec3 vertexCol;
+
+out vec3 colour;
+uniform mat4 MVP;
+
+void main() {
+    gl_Position = MVP * vec4(vertexPos, 1.0);
+    colour = vertexCol;
+}
